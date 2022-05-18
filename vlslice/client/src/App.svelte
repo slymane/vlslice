9d37a995ee82744af9a7f66e2b6273d23e71f5f2
Eric Slyman (he/they) <slymane@oregonstate.edu>
2022-05-17 18:15:24 -0700

    Add toy svelte app + flask server

---
+++ vlslice/client/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	let rand = -1;
+	function getRand() {
+	  fetch("./rand")
+		.then(d => d.text())
+		.then(d => (rand = d));
+	}
+  </script>
+
+<main>
+	<h1>Your number is {rand}!</h1>
+	<button on:click={getRand}>Get a random number</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ vlslice/server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, send_from_directory
+import random
+
+app = Flask(__name__)
+
+# Path for main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory("../client/public", "index.html")
+
+# Static files (compiles JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory("../client/public", path)
+
+# Display a random number
+@app.route("/rand")
+def hello():
+    return str(random.randint(0, 100))
+
+if __name__ == "__main__":
+    app.run(debug=True)
