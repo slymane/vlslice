45e1ac5675f56622cf94021dfd2082338c936521
slymane <slymane@oregonstate.edu>
2022-05-29 15:08:54 -0700

    Add cluster summary bars

--- vlslice/client/src/App.svelte
+++ vlslice/client/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+	import * as d3 from "d3";
+
 	// Filtering Variables
 	let fltrBaseline = null;
 	let fltrAugment = null;
 	let fltrK = null;
 	let enableFilter = true;
+
+	// Cluster Summary
+	let scaleMean = d3.scaleLinear().domain([0, 1]);
+	let scaleSize = d3.scaleLinear();
+	let scaleVar = d3.scaleLinear();
 
 	// Image store
 	let clusters = [];
@@ -11,9 +18,12 @@
 	function filter() {
 		if (fltrBaseline != null && fltrAugment != null && fltrK != null) {
 			console.log('Fetching filtered photos...')
+
+			// Disable filtering and clear clusters
 			enableFilter = false;
 			clusters = [];
 
+			// Fetch new clustering from the server
 			fetch('./filter', {
 				method: 'POST',
 				headers: {'Content-Type': 'Application/json'},
@@ -27,7 +37,23 @@
 			})
 			.then(r => (r.json()))
 			.then(function(jsonData) {
+				console.log('Asigning new clusters...')
 				clusters = jsonData;
+				console.log(clusters);
+			}).then(function() {
+				console.log('Scaling to new clusters...')
+
+				// Setup scaling for summary bars
+				let vMax = Math.max(...clusters.map(c => c.var));
+				let cMax = Math.max(...clusters.map(c => c.count));
+
+				let cw = document.getElementsByClassName('cluster-summary')[0].clientWidth;
+				let range = [0, 0.70 * cw]
+				scaleMean = scaleMean.range(range);
+				scaleVar = scaleVar.domain([0, vMax]).range(range);
+				scaleSize = scaleSize.domain([0, cMax]).range(range);
+
+				// Re-enable the filter
 				enableFilter = true;
 			});
 		} else {
@@ -82,22 +108,81 @@
 			<button class="btn" disabled="{enableFilter ? null : 'disabled'}" type="submit" on:click={filter}>Filter</button>
 		</div>
 	</div>
+	<br>
 
 	<!-- CLUSTER DISPLAY -->
 	{#each clusters as clstr (clstr.id)}
-		<div class="cluster gap-px" id="cluster-{clstr.id}">
-			{#each clstr.images as img (img.id)}
-				<img id="img-{img.id}" class="m-1 p-0" alt="Filtered dataset sample" on:click="{() => console.log(img.id)}"  
-					src="data:image/png;base64,{img.b64}" width="128" height="128"/>
-			{/each}
+		<div class="cluster w-full grid grid-cols-4">
+			<!-- Summary -->
+			<div class="cluster-summary col-span-1" bind>
+
+				<!-- Bars -->
+				<div id="summary-{clstr.id}" class="grid grid-rows-3 grid-cols-5 gap-x-4">
+					<div class="text-right">Mean</div>
+					<div class="col-span-4 summary-bar">
+						<svg width="100%" height="20">
+							<rect y="5" width="{scaleMean(Math.abs(clstr.mean))}" height="10" 
+								fill={clstr.mean > 0 ? "hsl(var(--su))" : "hsl(var(--er))"}
+							/>
+							<text dominant-baseline="middle" x="{scaleMean(Math.abs(clstr.mean)) + 5}" y="50%" fill="lightgray">
+								({clstr.mean > 0 ? '+' : ''}{clstr.mean.toFixed(2)})
+							</text>
+						</svg>
+					</div>
+
+					<div class="text-right">Variance</div>
+					<div class="col-span-4 summary-bar">
+						<svg width="100%" height="20">
+							<rect y="5" width="{scaleVar(clstr.var)}" height="10"/>
+							<text dominant-baseline="middle" x="{scaleVar(clstr.var) + 5}" y="50%" fill="lightgray">
+								({clstr.var.toFixed(2)})
+							</text>
+						</svg>
+					</div>
+
+					<div class="text-right">Size</div>
+					<div class="col-span-4 summary-bar">
+						<svg width="100%" height="20">
+							<rect y="5" width="{scaleSize(clstr.count)}px" height="10"/>
+							<text dominant-baseline="middle" x="{scaleSize(clstr.count) + 5}px" y="50%" fill="lightgray">
+								({clstr.count})
+							</text>
+						</svg>
+					</div>
+				</div>
+
+				<!-- Selector Buttons -->
+				<div>
+					TODO
+				</div>
+			</div>
+
+			<!-- Images -->
+			<div class="cluster-images col-span-3 gap-px" id="cluster-{clstr.id}">
+				{#each clstr.images as img (img.id)}
+					<img id="img-{img.id}" class="m-1 p-0" alt="Filtered dataset sample" on:click="{() => console.log(img.id)}"  
+						src="data:image/png;base64,{img.b64}" width="128" height="128"/>
+				{/each}
+			</div>
 		</div>
+
 	{/each}
 
 	</div>
 </main>
 
 <style>
-	.cluster {
+	.cluster-summary {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.summary-bar {
+		display: flex;
+		align-items: center;
+	}
+
+	.cluster-images {
 		display: flex;
 		flex-wrap: wrap;
 		border: 1px solid black;
