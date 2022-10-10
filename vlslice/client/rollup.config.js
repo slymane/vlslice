4f4bb70dd1e5069ccecd46b870da933ed641ae86
Eric Slyman <slymane@oregonstate.edu>
2022-10-10 19:30:41 +0000

    Remove bad tooling

--- vlslice/client/rollup.config.js
+++ vlslice/client/rollup.config.js
@@ -1,7 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 
@@ -61,10 +60,6 @@ export default {
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
 		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
