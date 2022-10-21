e14c8797fe78c98dd4de7748c64a231538b9e588
slymane <slymane@oregonstate.edu>
2022-10-21 09:17:39 -0700

    Fix drawer collapsing

--- vlslice/client/src/components/Section.svelte
+++ vlslice/client/src/components/Section.svelte
@@ -1,9 +1,11 @@
 <script>
+    import { slide } from 'svelte/transition';
     export let badge;
+    let checked = true;;
 </script>
 
 <div tabindex="0" class="collapse collapse-arrow">
-    <input type="checkbox" checked/> 
+    <input bind:checked type="checkbox"/> 
     <div class="collapse-title text-2xl font-medium">
         <div class="indicator" on:click="{(e) => e.target.parentElement.previousElementSibling.click()}">
             <span class="indicator-item badge badge-primary">{badge}</span> 
@@ -11,7 +13,7 @@
         </div>
         <div class="divider"></div>
     </div>
-    <div class="collapse-content no-max-height"> 
+    <div class="collapse-content" class:no-max-height={checked}> 
         <slot name="content"/>
     </div>
 </div>
